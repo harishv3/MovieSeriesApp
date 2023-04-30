5d647149b345c3e6094ae7b36c7ecd5ac0658f50
harish v <harishvm003@gmail.com>
2023-04-30 22:19:47 +0530

    COM: filtering issue fixes

--- src/components/movies/index.js
+++ src/components/movies/index.js
@@ -4,15 +4,16 @@ import { CardComponent } from "../card";
 import { map } from "lodash";
 
 export function MoviesComponent(){
-    const [ moviesList, setMoviesList ] = useState([]);
+    const [ moviesList, setMoviesList ] = useState(null);
     const [ showErrorState, setShowErrorState ] = useState(false);
 
     useEffect(()=>{
         try {
             let moviesData = SampleData.entries;
-            moviesData = moviesData.filter(data=>data.releaseYear>=2010).splice(0,21);
+            moviesData = moviesData.filter(data=>data.releaseYear>=2010);
             moviesData.sort((val1, val2) => (val1.title.toLowerCase() > val2.title.toLowerCase()) ? 1 : -1);
-            moviesData = moviesData.filter(data=>data.programType==="movies");
+            moviesData = moviesData.filter(data=>data.programType==="movie");
+            moviesData = moviesData.splice(0,21);
             setMoviesList(moviesData);
         } catch(error){
             setShowErrorState(true);
@@ -23,11 +24,11 @@ export function MoviesComponent(){
         <div>
             <header className="pageheader">Popular Movies</header>
             <div className="main">
-                { !showErrorState && moviesList.length>0 && map(moviesList, (moviesData, index)=>
+                { !showErrorState && moviesList?.length>0 && map(moviesList, (moviesData, index)=>
                     <CardComponent label={moviesData?.title} imgSrc={moviesData?.images?.["Poster Art"]?.url} key={index}/>
                 )}
-                { !showErrorState && !moviesList.length && <span>Loading...</span> }
-                { showErrorState && <span>Loading...</span> }
+                { !showErrorState && !moviesList?.length && <span>Loading...</span> }
+                { showErrorState && <span>Oops, something went wrong...</span> }
             </div>
         </div>
     )
--- src/components/series/index.js
+++ src/components/series/index.js
@@ -4,15 +4,16 @@ import { CardComponent } from "../card";
 import { map } from "lodash";
 
 export function SeriesComponent(){
-    const [ seriesList, setSeriesList ] = useState([]);
+    const [ seriesList, setSeriesList ] = useState(null);
     const [ showErrorState, setShowErrorState ] = useState(false);
 
     useEffect(()=>{
         try {
             let seriesData = SampleData.entries;
-            seriesData = seriesData.filter(data=>data.releaseYear>=2010).splice(0,21);
+            seriesData = seriesData.filter(data=>data.releaseYear>=2010);
             seriesData.sort((val1, val2) => (val1.title.toLowerCase() > val2.title.toLowerCase()) ? 1 : -1);
             seriesData = seriesData.filter(data=>data.programType==="series");
+            seriesData = seriesData.splice(0,21);
             setSeriesList(seriesData);
         } catch(error){
             setShowErrorState(true);
@@ -23,11 +24,11 @@ export function SeriesComponent(){
         <div>
             <header className="pageheader">Popular Series</header>
             <div className="main">
-                { !showErrorState && seriesList.length>0 && map(seriesList, (seriesData, index)=>
+                { !showErrorState && seriesList?.length>0 && map(seriesList, (seriesData, index)=>
                     <CardComponent label={seriesData?.title} imgSrc={seriesData?.images?.["Poster Art"]?.url} key={index}/>
                 )}
-                { !showErrorState && !seriesList.length && <span>Loading...</span> }
-                { showErrorState && <span>Loading...</span> }
+                { !showErrorState && !seriesList?.length && <span>Loading...</span> }
+                { showErrorState && <span>Oops, something went wrong...</span> }
             </div>
         </div>
     )
